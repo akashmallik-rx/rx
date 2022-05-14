ed3894a9b1c07e165cc236b9b485a1aa6e017b0f
akashmallik <toakashmallik@gmail.com>
2022-05-14 22:19:03 +0600

    NO-TICKET: API services added

---
+++ src/app/services/advice.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdviceService {
+  private baseUrl = environment.apiUrl + '/advices';
+
+  constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get(this.baseUrl);
+  }
+
+  get(id: number) {
+    return this.http.get(`${this.baseUrl}/${id}` + '/');
+  }
+
+  create(payload: any) {
+    return this.http.post(this.baseUrl, payload);
+  }
+
+  update(id: number, payload: any) {
+    return this.http.put(`${this.baseUrl}/${id}`, payload);
+  }
+
+  partialUpdate(id: number, payload: any) {
+    return this.http.patch(`${this.baseUrl}/${id}`, payload);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+}
